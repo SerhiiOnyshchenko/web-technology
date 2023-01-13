19056b99f2fb100b625f6d40a7514f775528cf48
Serhii Onyshchenko <62890013+SerhiiOnyshchenko@users.noreply.github.com>
2023-01-13 09:57:54 +0200

    fix modal and mobnav

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -1,18 +1,34 @@
 import BurgerMenuToggle from 'components/BurgerMenuToggle/BurgerMenuToggle';
 import Button from 'components/Button/Button';
+import Modal from 'components/Modal/Modal';
 import Nav from 'components/Nav/Nav';
+import { useState } from 'react';
 import s from './index.module.css';
 
 export default function MobileNav({ isOpen, setOpen }) {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
       <BurgerMenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
       {isOpen && (
         <div className={s.navWrapper}>
           <Nav toggle={() => setOpen(!isOpen)} />
-          <a className={s.Tel} href="tel:+380">
-            <Button active={true}>Request a call</Button>
-          </a>
+          <Button
+            onClick={() => {
+              setShowModal(true);
+            }}
+            active={true}
+          >
+            Request a call
+          </Button>
+          {showModal && (
+            <Modal
+              onClose={() => {
+                setShowModal(false);
+              }}
+            />
+          )}
         </div>
       )}
     </>
